cbf72c79ca43a85b755d094f49253c4bab81a7ec
Weiguang <chen.weiguang@gmail.com>
2018-06-27 17:20:17 +0800

    Initial implementation (#1)
    * Add index.js
    
    * Add gitignore
    
    * Code runs, but nothing observed
    
    * Working example, but not from offset
    
    * Set group id

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-# node-kafka-test
+# `node-kafka-test`
+
 Test consuming Kafka MQ set-up for Node.js
+
+## How to try
+
+Install `yarn` package manager first.
+
+Then run the following commands:
+
+```bash
+yarn install
+```
+
+```bash
+yarn start
+```
---
+++ index.js
@@ -0,0 +1,21 @@
+const TOPIC = "test";
+const GROUP_ID = "consumer"
+
+var kafka = require("kafka-node");
+
+// this uses Zookeepers 
+const client = new kafka.Client("localhost:2181");
+const topics = [{ topic: TOPIC, partition: 0 }]
+const options = { groupId: GROUP_ID, autoCommit: true, fetchMaxWaitMs: 500, fetchMaxBytes: 1024*1024, fromOffset: true };
+
+const consumer = new kafka.Consumer(client, topics, options);
+
+console.log(`Waiting for messages for topic '${TOPIC}'...`)
+
+consumer.on("message", msg => {
+  console.log(msg);
+});
+
+consumer.on("error", err => {
+  console.log(`Error: ${msg}`);
+});
