2e7e556a769cb25060dbc07cc80513904cc3cdd3
Chen Weiguang <chen.weiguang@gmail.com>
2018-06-27 17:18:32 +0800

    Working example, but not from offset

--- index.js
+++ index.js
@@ -2,11 +2,19 @@ const TOPIC = "test";
 
 var kafka = require("kafka-node");
 
-const client = new kafka.Client("localhost:9092");
-const consumer = new kafka.Consumer(client, [{ topic: TOPIC, partition: 0 }], {
-  autoCommit: true
-});
+// this uses Zookeepers 
+const client = new kafka.Client("localhost:2181");
+const topics = [{ topic: TOPIC, partition: 0 }]
+const options = { autoCommit: true, fetchMaxWaitMs: 500, fetchMaxBytes: 1024*1024, fromOffset: true };
+
+const consumer = new kafka.Consumer(client, topics, options);
+
+console.log(`Waiting for messages for topic '${TOPIC}'...`)
 
 consumer.on("message", msg => {
   console.log(msg);
+});
+
+consumer.on("error", err => {
+  console.log(`Error: ${msg}`);
 });
